d80ddc002e7ed14d69257ac26b4032e538a5385f
Alice <alice.berlose@gmail.com>
2021-05-05 18:06:23 +0200

    ParseProperties method implemented + last details

--- src/main/java/bme/agile/Parser.java
+++ src/main/java/bme/agile/Parser.java
@@ -23,35 +23,35 @@ public class Parser {
 
     public static void main(String[] args) {
     	
-		File logfile = new File(FILE_TEST);
+		File logfile = new File(FILE_1);
 		String[] filters = {SENT_ON, ENQUEUD_ON};
 		List<String> messagesList = new ArrayList<String>();
 		List<MessageProperty> messageProperties = new ArrayList<>();
-		ArrayList<MessageProperty> currentParents = new ArrayList<>(); // Used to track the current parent property of the properties we're parsing
+		ArrayList<MessageProperty> currentParents = new ArrayList<>();
 		
 		// Creating a list of PORTEVENT messages (type SENT_ON or ENQUEUED_ON)
 		messagesList = parseTxtFile(logfile, filters);
 		
+		// Extracting the properties of each message
 		messagesList.forEach((tempMessage) -> {
 			Reader inputString = new StringReader(tempMessage);
 	    	BufferedReader reader = new BufferedReader(inputString);
-        	Integer openBracketsNb = 0; // These will be used in parseProperties method to link properly the childrenProperties to their parentProperty
-        	Integer closedBracketsNb = 0;
 	    	try {
 	    		currentParents.add(new MessageProperty(PORTEVENT.toLowerCase(), ARRAY_STRUCTURE, new MessageProperty()));
 	    		messageProperties.add(currentParents.get(0));
 	    		// Parse the properties and obtain as a result a list of MessageProperty
 		    	reader.readLine();
-	    		parseProperties(reader, messageProperties, openBracketsNb, closedBracketsNb, currentParents);
+	    		parseProperties(reader, messageProperties, currentParents);
 				reader.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		});
+		
+		// Populating the list of children of each property (if needed)
 		messageProperties.forEach((temp) -> {
-			if (temp.getParentProperty().getPropertyName() != null) {
-				String nameParent = temp.getParentProperty().getPropertyName();
+			String nameParent = temp.getParentProperty().getPropertyName();
+			if (nameParent != null) {
 				boolean found = false;
 				Integer index = -1;
 				while (!found) {
@@ -63,17 +63,22 @@ public class Parser {
 			}
 		});
 		
-		// Debug printing - delete when project development is over
-		messageProperties.forEach((temp) -> {
-			temp.printProperty();
-		});
+		System.out.println(messageProperties.size());
 		
+		// Generating the classes at runtime
     	messageProperties.forEach((temp) -> {
-    		generateClassAtRuntime(temp);
+    		if (temp.getPropertyType().equals(ARRAY_STRUCTURE)) {
+    			generateClassAtRuntime(temp);
+    		}
     	});
     }
     
-    // Parsing a text file and returning a list of the PORTEVENT messages
+    /**
+     * Parsing a text file and returning a list of the PORTEVENT messages
+     * @param logfile
+     * @param filters
+     * @return
+     */
     public static ArrayList<String> parseTxtFile(File logfile, String[] filters){
     	BufferedReader reader;
     	ArrayList<String> messagesList = new ArrayList<String>();
@@ -107,45 +112,51 @@ public class Parser {
     	return messagesList;
     }
     
-    // Parsing the properties and returning a list of MessageProperty
-    public static List<MessageProperty> parseProperties(BufferedReader reader, List<MessageProperty> propertiesList, Integer openBracketsNb, Integer closedBracketsNb, ArrayList<MessageProperty> currentParents) throws IOException {
+    /**
+     * Parsing the properties and returning a list of MessageProperty
+     * @param reader
+     * @param propertiesList
+     * @param currentParents
+     * @return
+     * @throws IOException
+     */
+    public static List<MessageProperty> parseProperties(BufferedReader reader, List<MessageProperty> propertiesList, ArrayList<MessageProperty> currentParents) throws IOException {
     	String currentLine = reader.readLine();
     	
     	while (currentLine != null) {
-    		if (openBracketsNb == closedBracketsNb 
-    				&& !((MessageProperty)(currentParents.get(currentParents.size() - 1))).getPropertyName().equals(PORTEVENT.toLowerCase())) {
-    			currentParents.remove(currentParents.size() - 1);
-    		}
-    		if(currentLine.contains(OPEN_BRACKET))
-				openBracketsNb++;
-			if(currentLine.contains(CLOSED_BRACKET))
-				closedBracketsNb++;
-    		if (currentLine.contains(":=")) {
+    		if (currentLine.contains(EQUAL)) {
     			MessageProperty property = new MessageProperty (
-						currentLine.trim().substring(0, currentLine.trim().indexOf(":=")),
-						checkType(currentLine.substring(currentLine.indexOf(":=") + 2)),	
-						(currentParents.isEmpty() ? new MessageProperty() : currentParents.get(currentParents.size() - 1)));
-    			
-				if (!checkIfExists(property, propertiesList)) {
-					propertiesList.add(property);
-				}
-    			if (property.getPropertyType().equals(ARRAY_STRUCTURE)) {
-    				openBracketsNb = 1;
-	        		closedBracketsNb = 0;
-    				currentParents.add(property);
-    				parseProperties(reader, propertiesList, openBracketsNb, closedBracketsNb, currentParents).forEach((temp) -> {
-        				if (!checkIfExists(property, propertiesList)) {
-        					propertiesList.add(property);
-        				}
-    				});
-    			}
+    				currentLine.trim().substring(0, currentLine.trim().indexOf(EQUAL)),
+    				checkType(currentLine.substring(currentLine.indexOf(EQUAL) + 2)),	
+    				(currentParents.get(currentParents.size() - 1)));
+    		
+    			Integer indexProperty = checkIfExists(property, propertiesList);
+				if (indexProperty == -1)
+	    			propertiesList.add(property);
+				else
+					if (propertiesList.get(indexProperty).getPropertyType() == OBJECT)
+						propertiesList.get(indexProperty).setPropertyType(property.getPropertyType());
+					
+	    		if (property.getPropertyType().equals(ARRAY_STRUCTURE))
+	    			currentParents.add(property);
     		}
-			currentLine = reader.readLine();
+    		
+    		if (currentLine.trim().equals(OPEN_BRACKET))
+    			currentParents.add(currentParents.get(currentParents.size() - 1));
+
+    		if (currentLine.trim().contains(":={}") || currentLine.trim().equals("}") || currentLine.trim().equals("},"))
+    			currentParents.remove(currentParents.size() - 1);
+    		
+    		currentLine = reader.readLine();
     	}
     	return propertiesList;
     }
     
-    //Checking the type of a property
+    /**
+     * Checking the type of a property
+     * @param propertyValue
+     * @return
+     */
     public static String checkType(String propertyValue) {
     	if (propertyValue.contains("\""))
     		return STRING;
@@ -163,25 +174,38 @@ public class Parser {
 		}
     }
     
-    // Check if a property already is in a list of properties
-    public static boolean checkIfExists(MessageProperty property, List<MessageProperty> propertiesList) {
+    /**
+     * Checking if a property already is in a list of properties
+     * @param property
+     * @param propertiesList
+     * @return
+     */
+    public static Integer checkIfExists(MessageProperty property, List<MessageProperty> propertiesList) {
     	Boolean alreadyExists = false;
-		Integer index = 0;
-		while (alreadyExists == false && index < propertiesList.size()) {
-			alreadyExists = property.compareProperties(property, propertiesList.get(index));
+		Integer index = -1;
+		while (alreadyExists == false && index < propertiesList.size() - 1) {
 			index++;
+			alreadyExists = property.compareProperties(property, propertiesList.get(index));
 		}
-		return alreadyExists;
+		return (alreadyExists ? index : -1);
     }
 
-    // Checking if a string 'inputStr' contains the elements of an array of string 'items'
+    /**
+     * Checking if a string 'inputStr' contains the elements of an array of string 'items'
+     * @param inputStr
+     * @param items
+     * @return
+     */
     public static boolean stringContainsItemFromList(String inputStr, String[] items) {
         return Arrays.stream(items).anyMatch(inputStr::contains);
     }
     
-    //Generate a class at runtime
+    /**
+     * Generating a class at runtime
+     * @param messageProperty
+     */
     public static void generateClassAtRuntime(MessageProperty messageProperty) {
-    	if (messageProperty.getPropertyType().equals(ARRAY_STRUCTURE)) {
+    	
         	UnitSourceGenerator unitSG = UnitSourceGenerator.create("generatedclasses"
         		).addImport(
         			"java.util.Arrays"
@@ -206,8 +230,5 @@ public class Parser {
     		
     		unitSG.addClass(tmpClass);
     		unitSG.storeToClassPath(System.getProperty("user.dir") + "/src/main/java");
-    		// Debug printing - delete when project development is over
-    		System.out.println("\nGenerated code:\n" + unitSG.make());
-    	};
     }
 }
