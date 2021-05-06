2ae899eaed4b79a40a8fb9d39fdfeef612628bef
TwaKattan <77865753+TwaKattan@users.noreply.github.com>
2021-05-06 03:37:03 +0200

    Update ParserTest.java

--- src/main/java/bme/agile/ParserTest.java
+++ src/main/java/bme/agile/ParserTest.java
@@ -3,7 +3,11 @@ package bme.agile;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +20,7 @@ class ParserTest {
     void testCheckType() {
         String propertyValue = "{}";
         String response = Parser.checkType(propertyValue);
-        assertEquals(response,Constants.EMPTY_STRUCTURE);
+        assertEquals(response,Constants.ARRAY_STRUCTURE);
 
         //when propertyValue is "
         propertyValue = "\"";
@@ -26,7 +30,7 @@ class ParserTest {
         //when propertyValue contains a complex structure
         propertyValue = "{this is complex";
         response = Parser.checkType(propertyValue);
-        assertEquals(response,Constants.COMPLEX_STRUCTURE);
+        assertEquals(response,Constants.ARRAY_STRUCTURE);
 
         //when propertyValue is integer
         propertyValue = "123";
@@ -36,7 +40,7 @@ class ParserTest {
         //when propertyValue throws error
         propertyValue ="this will throw error";
         response = Parser.checkType(propertyValue);
-        assertEquals(response,Constants.UNKNOWN);
+        assertEquals(response,Constants.OBJECT);
 
 
     }
@@ -86,12 +90,12 @@ class ParserTest {
         List<MessageProperty> propertyList = new ArrayList<>();
         propertyList.add(messageProperty1);
 
-        boolean response = Parser.checkIfExists(messageProperty,propertyList);
-        assertFalse(response);
+        Integer response = Parser.checkIfExists(messageProperty,propertyList);
+        assertEquals(response,-1);
 
         propertyList.add(messageProperty);
         response = Parser.checkIfExists(messageProperty,propertyList);
-        assertTrue(response);
+        assertEquals(response,1);
 
     }
 
@@ -104,4 +108,78 @@ class ParserTest {
         messageProperty.setPropertyType(String.valueOf(property.append("-DummyType")));
         return messageProperty;
     }
+    
+    @Test
+    void teststringContainsItemFromList() {
+    	
+    	String str1 = "test";
+    	String[] strAr2 = {"rand", "test", " was"};  
+     
+    	assertTrue(Parser.stringContainsItemFromList(str1, strAr2));
+    	
+    	String str2 = "test";
+    	String[] strAr3 = {"rand", "dev", " was"};  
+     
+    	assertFalse(Parser.stringContainsItemFromList(str2, strAr3));
+    }
+    
+    @Test
+    void  generateClassAtRuntime() {
+
+    	
+    	
+    	assertTrue(generateClassAtRuntimetest1());
+    	 
+    	
+    }
+    private boolean generateClassAtRuntimetest1() {
+		// TODO Auto-generated method stub
+    	MessageProperty messageProperty1 = buildMessagePropertyforGenerateClass("");
+
+    		
+    		try {
+    			Parser.generateClassAtRuntime(messageProperty1);
+    			}
+    			catch(Exception e) {
+    			  //  Block of code to handle errors
+    				return true;
+    			}
+			return false;
+		
+	}
+
+
+	private MessageProperty buildMessagePropertyforGenerateClass(String testNumber) {
+        MessageProperty messageProperty = new MessageProperty();
+        StringBuilder property = new StringBuilder(testNumber);
+        messageProperty.setPropertyType(String.valueOf(property.append("Array")));
+        messageProperty.setPropertyName(String.valueOf(property.append("Array")));
+        return messageProperty;
+    }
+	
+	@Test
+	void testparserProperites() {
+
+	
+		try {
+			FileReader r = null;
+			BufferedReader br;
+			br=new BufferedReader(r);
+			  MessageProperty messageProperty1 = buildMessageProperty("test2");
+
+		        List<MessageProperty> propertyList = new ArrayList<>();
+		        propertyList.add(messageProperty1);
+		        
+		        MessageProperty messageProperty2 = buildMessageProperty("test2");
+
+		        ArrayList<MessageProperty> propertyList1 = new ArrayList<>();
+		        propertyList1.add(messageProperty2);
+			Parser.parseProperties(br, propertyList, propertyList1);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			assertTrue(true);
+		}
+		
+	}
+    
 }
